52c8e01ab9875d753988d63d3944c67502b8357d
HaoTong Zhi <>
2018-09-02 19:47:32 +1000

    modify app.js

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var express =require('express');
 var todoController=require('./controllers/todoControllers');
+var http=require('http');
+var debug = require('debug')('express-todo-zhi:server');
 
 var app=express();
 app.set('view engine','ejs');
@@ -7,6 +9,34 @@ app.use(express.static('./public'));
 
 todoController(app);
 
-app.listen(3000);
+var port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+var server = http.createServer(app);
+server.listen(port);
+server.on('listening', onListening);
+// app.listen(3000);
 
-console.log('listing port 3000');
+// console.log('listing port 3000');
+function normalizePort(val) {
+    var port = parseInt(val, 10);
+  
+    if (isNaN(port)) {
+      // named pipe
+      return val;
+    }
+  
+    if (port >= 0) {
+      // port number
+      return port;
+    }
+  
+    return false;
+  }
+
+  function onListening() {
+    var addr = server.address();
+    var bind = typeof addr === 'string'
+      ? 'pipe ' + addr
+      : 'port ' + addr.port;
+    debug('Listening on ' + bind);
+  }
